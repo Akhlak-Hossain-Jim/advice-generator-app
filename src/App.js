807ebfc3ec7dc82f0f6e0be240d980c8c56b4337
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-03-06 01:51:37 +0600

    🛠done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Style } from "./Style";
 
 function App() {
+  const [advice, setAdvice] = useState();
+  const [id, setId] = useState(true);
+  const [dice, setDice] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+        setId(data.slip.id);
+      })
+      .catch((err) => console.log(err));
+  }, [dice]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Style>
+      <div className="card">
+        <h1>ADVICE #{id}</h1>
+        <q>&nbsp;{advice}&nbsp;</q>
+        <img src="/pattern-divider-desktop.svg" alt="divider" />
+        <button onClick={() => setDice(!dice)}>
+          <img src="/icon-dice.svg" alt="dice" />
+        </button>
+      </div>
+    </Style>
   );
 }
 
